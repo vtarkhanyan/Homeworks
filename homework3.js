7e11c5b11dcc9779fd04b99dc8fc194f6e19926b
vtarkhanyan <vtarkhanyan@gmail.com>
2020-04-10 00:10:53 +0400

    Update homework3.js

--- homework3.js
+++ homework3.js
@@ -1,1 +1,15 @@
 
+let num  = 4211133
+let numLength = num.toString().length-1
+function print(n,x){
+   
+       if(n.toString()[x]%2==0){
+            return false
+        }else if(n.toString()[x]==undefined){
+      return true
+        }
+                return print(n,x-1)
+  
+     }
+  
+  console.log(print(num,numLength))
