54975ce597d84036ffd0817f3eb60af4def5180a
vtarkhanyan <vtarkhanyan@gmail.com>
2020-04-11 16:50:01 +0400

    Update Homework 3 draft.js

--- Homework 3 draft.js
+++ Homework 3 draft.js
@@ -1,1 +1,14 @@
 
+function print(n){
+    let y = n%10
+    if(n>9){
+      n = Math.floor(n/10)
+      y +=  print(n)
+      }
+      if(y>9){
+        n=y
+        return print(n)
+        }
+          return y 
+   }
+console.log(print(1235652))
