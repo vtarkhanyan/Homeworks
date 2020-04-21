14180b4070979c6c8c16c257c3a9eef7d0375213
vtarkhanyan <vtarkhanyan@gmail.com>
2020-04-21 21:32:24 +0400

    Update Homework4.js

--- Homework4.js
+++ Homework4.js
@@ -1,3 +1,21 @@
+/* 1. Given an array. Write a recursive function that removes the first element and returns
+the given array. (without using arr.unshift(),assign second element to first, third element
+to second...)*/
+function print(arr,i){
+    arr[i]=arr[i+1]
+    if(i>=arr.length-1){
+        arr.length = arr.length-1
+        return arr
+      } 
+    return print(arr,++i)
+  }
+  console.log(print([1,2,3,4,5,6],0))
+
+
+
+
+
+
 let treeNode = [
     {parent: null, id: 0},
     {parent: 0, id: 1},
