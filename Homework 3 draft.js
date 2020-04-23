114f4c65a78de6565ff14f2b08ab29703e1d57cf
vtarkhanyan <vtarkhanyan@gmail.com>
2020-04-24 00:53:22 +0400

    Update Homework 3 draft.js

--- Homework 3 draft.js
+++ Homework 3 draft.js
@@ -14,3 +14,43 @@ function print(n){
           return y 
    }
 console.log(print(1235652))
+
+
+unction print(arr, n){
+let obj = {}
+let obj1 = {}
+  for(let  i = 1; i<n+1; i++){
+    obj[i] = i-1
+    }
+    
+    Object.assign(obj1,obj)
+  
+  
+    let arr1 =[]
+    let x = n-1
+      for(let i = 0, a = x-1; i<5; i++){
+   
+          for(let j = n-1; j<arr.length; j++){
+          
+              for(let key in obj){
+                arr1.push(arr[obj[key]])
+            }
+            obj[n] += 1
+            
+        }
+        Object.assign(obj,obj1)
+ 
+        for(let key in obj){
+
+         if(key>a)
+         obj[key]+=1
+         }
+ 
+      }
+      
+    
+    return arr1
+  
+  }
+
+console.log(print([1,2,3,4,5],4))
