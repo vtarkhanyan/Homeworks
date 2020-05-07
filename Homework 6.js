d85c6253d7443f5451bf4ab6ec364d717f74a241
vtarkhanyan <vtarkhanyan@gmail.com>
2020-05-07 21:29:53 +0400

    Update Homework 6.js

--- Homework 6.js
+++ Homework 6.js
@@ -10,3 +10,11 @@ function uniqueElement(arr){
   return true
   }
   console.log(uniqueElement([1,[7],2,3,22,[5],6,"1",1,[7],222]))
+ 
+/*2. Given an array of numbers. Find the sum of numbers’ quadratic which are even.*/
+function sumEven(arr){
+    let sum  = 0
+    arr.map(x=>Math.sqrt(x)).filter(x=>x%2==0).map(a=>sum+=a)
+    return sum
+  }
+  console.log(sumEven([4,9,16,25,36,49,64,81,100]))
