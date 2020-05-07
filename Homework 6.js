80fafc326a6fb7b5ff86f8e2157f37e6e1291cd1
vtarkhanyan <vtarkhanyan@gmail.com>
2020-05-07 21:17:27 +0400

    Update Homework 6.js

--- Homework 6.js
+++ Homework 6.js
@@ -1,14 +1,12 @@
 /*1 Given an array. Determine whether it consists only from unique elements or not.*/
+
 function uniqueElement(arr){
-    for(let i = 0; i<arr.length; i++){
-        for(let j = i+1; j<arr.length; j++){
-              if(arr[i]===arr[j]){
-                return false
-                }
-          }
+    arr.sort((a,b)=>a-b)
+    for(let i = 0; i<arr.length-1; i++){
+        if(arr[i]===arr[i+1]){
+           return false
+        }
       }
   return true
   }
-  
-  console.log(uniqueElement([1,2,3,22,5,6,7,7]))
-  
+  console.log(uniqueElement([1,[7],2,3,22,[5],6,"1",1,[7],222]))
