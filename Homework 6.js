4f90bc6d3e63feac800dc998845034d32f35c93a
vtarkhanyan <vtarkhanyan@gmail.com>
2020-05-07 20:45:23 +0400

    Update Homework 6.js

--- Homework 6.js
+++ Homework 6.js
@@ -2,7 +2,7 @@
 function uniqueElement(arr){
     for(let i = 0; i<arr.length; i++){
         for(let j = i+1; j<arr.length; j++){
-              if(arr[i]==arr[j]){
+              if(arr[i]===arr[j]){
                 return false
                 }
           }
